50c48e997d30ac48eeaed2ab41c8baec243b90a7
jello261 <42743725+jello261@users.noreply.github.com>
2021-04-19 21:54:56 -0400

    New Gui layout

--- TKGui.py
+++ TKGui.py
@@ -17,23 +17,33 @@ def display(window):
     clawClose = tk.Button(text="Close Claw", command=lambda :robotArm.closeClaw())
 
     #entry and labels for the base and arms
-    baseLabel = tk.Label(text="Base rotaion 0-90 130-650")
+    baseLabel = tk.Label(text="Base rotaion 0-90")
     base = tk.Entry(width=10)
 
-    xarmLabel = tk.Label(text="First Arm 0-90 130-650")
+    xarmLabel = tk.Label(text="First Arm 0-90")
     xarm = tk.Entry(width=10)
 
-    sxarmLabel = tk.Label(text="Second Arm 0-90 130-650")
+    sxarmLabel = tk.Label(text="Second Arm 0-90")
     sxarm = tk.Entry(width=10)
 
-    txarmLabel = tk.Label(text="Thrid Arm 0-90 130-650")
+    txarmLabel = tk.Label(text="Thrid Arm 0-90")
     txarm = tk.Entry(width=10)
+
+    xLabel = tk.Label(text="X Point")
+    x = tk.Entry(width=10)
+    
+    yLabel = tk.Label(text="Y Point")
+    y = tk.Entry(width=10)
+
+    phiLabel = tk.Label(text="Angle of the last arm")
+    phi = tk.Entry(width=10)
 
     #buttons to move the base and arms
     moveBase = tk.Button(text="Move Base", command=lambda :robotArm.baseMover(base))
     moveXarm = tk.Button(text="Move First Arm", command=lambda :robotArm.xarmMover(xarm))
     moveSXarm = tk.Button(text="Move Second Arm", command=lambda :robotArm.sxarmMover(sxarm))
     moveTXarm = tk.Button(text="Move Thrid Arm", command=lambda :robotArm.txarmMover(sxarm))
+    moveKin = tk.Button(text="Move arm to X, Y Potions", command=lambda :robotArm.inverseKiematics(x, y, phi))
 
     #display Buttons and entry's
     clawOpen.grid(row=0 ,column=0)
@@ -54,7 +64,17 @@ def display(window):
     moveSXarm.grid(row=3, column=2)
     moveTXarm.grid(row=3, column=3)
 
-    homeButton.grid(row=4, column=0)
+    xLabel.grid(row=4, column=0)
+    yLabel.grid(row=4, column=1)
+    phiLabel.grid(row=4, column=2)
+
+    x.grid(row=5, column=0)
+    y.grid(row=5, column=1)
+    phi.grid(row=5, column=2)
+
+    moveKin.grid(row=6, column=0)
+
+    homeButton.grid(row=7, column=0)
     #aps3conButton.grid(row=4, column=1)
 
     window.mainloop()
